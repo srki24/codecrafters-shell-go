66e578572ba26aa18cacd500b9ba1285e5700207
srki24 <25962029+srki24@users.noreply.github.com>
2024-06-19 18:27:59 +0200

    missing commands

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -3,12 +3,22 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
+	var text string
+
+	_, err := fmt.Scanln(&text)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s: command not found", text)
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
 }
