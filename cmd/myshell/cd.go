47287cc16ca703808b1af0b0da9776b2c2add323
srki24 <25962029+srki24@users.noreply.github.com>
2024-06-22 14:23:07 +0200

    missing colon

--- cmd/myshell/cd.go
+++ cmd/myshell/cd.go
@@ -21,7 +21,7 @@ func go_cd(argv ...string) {
 	err := os.Chdir(path)
 
 	if err != nil {
-		fmt.Printf("cd %s: No such file or directory\n", path)
+		fmt.Printf("cd: %s: No such file or directory\n", path)
 	}
 
 }
