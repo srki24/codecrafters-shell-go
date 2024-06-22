d47f41bea6d4f7be7cf5ea79f49f32588d0823f4
srki24 <25962029+srki24@users.noreply.github.com>
2024-06-22 13:10:48 +0200

    bugfix

--- cmd/myshell/executables.go
+++ cmd/myshell/executables.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 	"os/exec"
@@ -9,14 +10,17 @@ import (
 
 func getExecutablePath(cmd string) (path string, ok bool) {
 
-	ok = false
-
 	paths := filepath.SplitList(os.Getenv("PATH"))
 	for _, p := range paths {
 		fp := filepath.Join(p, cmd)
 
-		if _, err := exec.LookPath(fp); err == nil {
-			path = p
+		_, err := exec.LookPath(fp)
+		if errors.Is(err, exec.ErrDot) {
+			err = nil
+		}
+
+		if err == nil {
+			path = fp
 			ok = true
 			break
 		}
@@ -28,6 +32,10 @@ func getExecutablePath(cmd string) (path string, ok bool) {
 func executeCmd(cmd string) {
 
 	c := exec.Command(cmd)
+	if errors.Is(c.Err, exec.ErrDot) {
+		c.Err = nil
+	}
+
 	if err := c.Run(); err != nil {
 		log.Fatal(err)
 	}
